e574d61ddc79081d7ff6442d67367ed7e1a6763b
arthur <arthurdedsec@gmail.com>
2025-07-25 14:21:41 -0300

    Fix: erros de estilização

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ const inter = Inter({
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['100', '200', '400', '500', '600', '700'],
   display: 'swap',
 });
 
