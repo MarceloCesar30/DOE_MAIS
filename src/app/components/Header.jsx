2f21aa0e8795ebb76442b9ddf36637c83545fec6
Urbano4dev <urbano10.dev@gmail.com>
2025-07-25 12:15:12 -0300

    branch header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,45 @@
+import Link from 'next/link';
+import { FaHeartbeat } from "react-icons/fa";
+import styles from './Header.module.css';
 export default function Header() {
     return (
         <div>
 
+    <header className={styles.headerContainer}>
+      <Link href="/" legacyBehavior>
+        <a className={styles.logoContainer}>
+          <span className={styles.logoIcon}><FaHeartbeat /></span>
+          <span className={styles.logoText}>DOE+</span>
+        </a>
+      </Link>
+
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <Link href="/para-doadores">Para Doadores</Link>
+          </li>
+          <li>
+            <Link href="/para-hemocentros">Para Hemocentros</Link>
+          </li>
+          <li>
+            <Link href="/unidades">Unidades</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.actionButtons}>
+        <Link href="/login" passHref>
+          <button className={`${styles.btn} ${styles.btnSecondary}`}>Entrar</button>
+        </Link>
+        <Link href="/cadastro" passHref>
+          <button className={`${styles.btn} ${styles.btnPrimary}`}>Cadastrar</button>
+        </Link>
+      </div>
+    </header>
+  ;
+;
+
+export default Header;
         </div>
     )
 }
