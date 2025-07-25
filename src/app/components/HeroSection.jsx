c83126bcf0619b004c3f91301adfc568fb8beed5
arthur <arthurdedsec@gmail.com>
2025-07-25 11:40:30 -0300

    Wip: ajustes na hero section antes de merge da main

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,13 @@
 export default function HeroSection() {
     return (
-        <div>
+        <div className="flex items-center justify-center mt-80">
+            <div>
+                <h1 className="text-3xl">Conectando Vidas,  <span className="text-red-500">Salvando Histórias</span></h1>
+            </div>
 
+            <div>
+
+            </div>
         </div>
     )
 }
