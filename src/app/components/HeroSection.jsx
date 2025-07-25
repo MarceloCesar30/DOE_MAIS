3d9c65f5eadab4bebad9c1289604e13ea072a2de
arthur <arthurdedsec@gmail.com>
2025-07-25 11:55:54 -0300

    setup inicial da hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,10 @@
 export default function HeroSection() {
     return (
-        <div className="flex items-center justify-center">
-            <h1 className="text-3xl">Conectando Vidas,  <span className="text-red-500">Salvando Histórias</span></h1>
+        <div className="flex items-center justify-center mt-80">
+            <div className="flex-col">
+                <h1 className="text-3xl font-bold">Conectando Vidas,  <span className="text-red-500">Salvando Histórias</span></h1>
+                <p className="text-center font-light">A plataforma que conecta doadores de sangue com hemocentros de forma inteligente, <br /> facilitando doações e salvando vidas através da tecnologia.</p>
+            </div>
         </div>
     )
 }
