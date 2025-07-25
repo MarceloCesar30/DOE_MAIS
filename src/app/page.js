917c40394e39beccd98ad418ad937395a5aeeae4
arthur <arthurdedsec@gmail.com>
2025-07-25 11:08:25 -0300

    Fix: erro de importação arrumado

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 
 import Header from "./components/Header.jsx";
-import HeroSection from "./components/herosection";
+import HeroSection from "./components/HeroSection.jsx";
 
 export default function Home() {
   return (
