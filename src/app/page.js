c1a7196a24ec8f6e8b4eb34dd7a5504394d4c594
Urbano4dev <urbano10.dev@gmail.com>
2025-07-26 18:23:19 -0300

    FunctionalitySection adicionada com sucesso

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 
 import Header from "./components/Header.jsx";
 import HeroSection from "./components/HeroSection.jsx";
-
+import StatsSection from "./components/StatsSection.jsx";
+import FunctionalitySection from "./components/FunctionalitySection.jsx";
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <StatsSection />
+      <FunctionalitySection/>
     </div>
   );
 }
