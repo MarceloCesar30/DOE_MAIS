737288910bdb264c8936516b1814d2817f287d6e
Urbano4dev <urbano10.dev@gmail.com>
2025-07-26 18:56:55 -0300

    footer section adicionada

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Header from "./components/Header.jsx";
 import HeroSection from "./components/HeroSection.jsx";
 import StatsSection from "./components/StatsSection.jsx";
 import FunctionalitySection from "./components/FunctionalitySection.jsx";
+import Footer from "./components/Footer.jsx";
 export default function Home() {
   return (
     <div>
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <StatsSection />
       <FunctionalitySection/>
+      <Footer/>
     </div>
   );
 }
